98490ee4f662b7e4184dfcfb8ef0ec8b3063921c
Cleonildo Jr <cleu.junior@gmail.com>
2022-08-31 12:04:42 -0300

    OpenFeign

--- src/main/java/br/com/cloud/integration/external/model/AbilitySummary.java
+++ src/main/java/br/com/cloud/integration/external/model/AbilitySummary.java
@@ -1,10 +1,13 @@
 package br.com.cloud.integration.external.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class AbilitySummary {
-    private boolean is_hidden;
+
+    @JsonProperty("is_hidden")
+    private boolean isHidden;
     private int slot;
     private AbilityDescriptionSummary ability;
 }
--- src/main/java/br/com/cloud/integration/external/model/PokemonSummary.java
+++ src/main/java/br/com/cloud/integration/external/model/PokemonSummary.java
@@ -1,14 +1,17 @@
 package br.com.cloud.integration.external.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
 public class PokemonSummary {
-
     private int id;
     private String name;
+
+    @JsonProperty("base_experience")
+    private int baseExperience;
     private int order;
     private List<AbilitySummary> abilities;
 }
